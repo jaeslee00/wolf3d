03de2be218129642e77599a1ecdccc974aea1486
Jae Seok Lee <jslee062749@gmail.com>
2019-08-26 17:23:24 +0200

    DOOR is there ! event like 'open door' needs to be handle separately from movement... added NOTE to movement.c raycasting to recognize door needs a slight tweak in formula still..

--- srcs/movement.c
+++ srcs/movement.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   movement.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: viccarau <viccarau@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jaelee <jaelee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/23 03:05:39 by viccarau          #+#    #+#             */
-/*   Updated: 2019/08/25 09:50:06 by viccarau         ###   ########.fr       */
+/*   Updated: 2019/08/26 17:12:32 by jaelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ void	set_flag(t_wolf *wolf, SDL_Event event)
 	check_key(wolf, event, SDL_SCANCODE_EQUALS, 1UL << 4);
 	check_key(wolf, event, SDL_SCANCODE_MINUS, 1UL << 5);
 	check_key(wolf, event, SDL_SCANCODE_LSHIFT, 1UL << 6);
+	check_key(wolf, event, SDL_SCANCODE_F, 1UL << 7);
 }
 
 int		direction_movement(t_wolf *wolf, int **map, int framedelta)
@@ -62,7 +63,7 @@ int		direction_movement(t_wolf *wolf, int **map, int framedelta)
 
 	fov = fov_calculator(wolf);
 	p = &wolf->player;
-	time = (float)(framedelta / 100.f) * p->speed;
+	time = (float)(framedelta / 200.f) * p->speed;
 	if (wolf->flag & UP)
 	{
 		if (map[(int)(p->position.x + p->direction.x * (time + 0.2f))][(int)(p->position.y)] == 0)
@@ -107,5 +108,25 @@ int		direction_movement(t_wolf *wolf, int **map, int framedelta)
 	wolf->player.speed = 1.5f;
 		else
 		wolf->player.speed = 1;
+	//NOTE (jae) : testing open/closing doors coordinates are hardcoded lul some keys cannot be like movement i think!! dunno where to change though
+	if (wolf->flag & 1UL << 7)
+	{
+		if (map[5][5] == 3
+			&& (((int)p->position.x == 5 && (int)p->position.y == 6)
+			|| ((int)p->position.x == 5 && (int)p->position.y == 4)))
+			map[5][5] = 0;
+		else if (map[5][5] == 0
+			&& (((int)p->position.x == 5 && (int)p->position.y == 6)
+			|| ((int)p->position.x == 5 && (int)p->position.y == 4)))
+			map[5][5] = 3;
+		else if (map[9][5] == 3
+			&& (((int)p->position.x == 9 && (int)p->position.y == 6)
+			|| ((int)p->position.x == 9 && (int)p->position.y == 4)))
+			map[9][5] = 0;
+		else if (map[9][5] == 0
+			&& (((int)p->position.x == 9 && (int)p->position.y == 6)
+			|| ((int)p->position.x == 9 && (int)p->position.y == 4)))
+			map[9][5] = 3;
+	}
 	return (1);
 }
