cfff783c3c92c54e39bb20f23748f2613803b6be
viccarau <viccarau@student.42.fr>
2019-08-27 20:57:07 +0200

    fixed the door and pushed

--- srcs/debug.c
+++ srcs/debug.c
@@ -40,7 +40,8 @@ sint32 print_map(sint8 **map, t_obj obj, t_player *player, t_door *doors, t_wolf
 			else if (map[i][j] == 3 || map[i][j] == 5)
 			{
 				if (map[i][j] == 3)
-				doors[k].flag |= 1UL << 1;
+					doors[k].flag |= 1UL << 1;
+				doors[k].flag |= 1UL;
 				doors[k].pos.x = i;
 				doors[k].pos.y = j;
 				k++;
