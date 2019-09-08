fd458901a5053d729edfcab74582b987f5fc88e5
Jae Seok Lee <jslee062749@gmail.com>
2019-09-08 17:38:02 +0200

    pulled one pre_calculation out of the while_loop in texture_map() in draw_wall.c

--- srcs/draw_wall.c
+++ srcs/draw_wall.c
@@ -6,7 +6,7 @@
 /*   By: jaelee <jaelee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/27 22:57:14 by jaelee            #+#    #+#             */
-/*   Updated: 2019/09/08 05:33:54 by jaelee           ###   ########.fr       */
+/*   Updated: 2019/09/08 17:37:13 by jaelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ sint32	lighting(sint32 color, f32	distance)
 {
 	f32		light;
 
-	if (distance < 3)
+	if (distance < 3.0f)
 		return (color);
 	else
-		light = (1.0 - distance / 12.0);
-	if (light < 0.0)
-		light = 0.0;
-	if (light > 1.0)
-		light = 1.0;
-	return (rgb_lerp(0.0, light + 0.19f, color));
+		light = (1.0f - distance / 12.0f);
+	if (light < 0.0f)
+		light = 0.0f;
+	if (light > 1.0f)
+		light = 1.0f;
+	return (rgb_lerp(0, light + 0.19f, color));
 }
 
 sint8	texture_pick(t_raycaster *ray)
@@ -38,25 +38,26 @@ sint8	texture_pick(t_raycaster *ray)
 }
 
 void	texture_map(t_wolf *wf, t_texture_map *tex_map, sint32 x,
-				t_raycaster *ray, t_texture *tex)
+				f32 perp_dist, t_texture *tex)
 {
 	sint32	tex_height_scale;
 	sint32	color;
 	sint32	tex_y;
 	sint32	y_offset;
 	sint32	y;
-(void)ray;
+	sint32 precalc;
+	
+	precalc = (tex->height << 16) / tex_map->column_height;
 	y_offset = ((tex_map->column_height - H) >> 1) + wf->view;
-	y = tex_map->start;
+	y = tex_map->start + 1;
 	while (y < tex_map->end)
 	{
 		tex_height_scale = y + y_offset;
-		//TODO (jae) : "tex->height) / tex_map.column_height" might be pulled out from the loop
-		tex_y = (tex_height_scale * tex->height) / tex_map->column_height;
+		tex_y = (tex_height_scale * precalc) >> 16;
 		color = tex->data[tex->width * tex_y + tex_map->coord.x];
 		//NOTE (jae) : lighting in ray_casting seems quite expensive. Needs to check!!
-		wf->img[x + y * W] = color; //lighting(color, ray->perp_dist);
-		y += 1;
+		wf->img[x + y * W] = lighting(color, perp_dist);
+		y++;
 	}
 }
 
@@ -80,5 +81,5 @@ void	draw_wall(t_wolf *wf, sint32 line_height, sint32 x, t_raycaster *ray)
 		tex_width_scale = wf->player->pos.x + ray->perp_dist * wf->player->ray.x;
 	tex_width_scale -= floor(tex_width_scale);
 	tex_map.coord.x = (sint32)(tex_width_scale * (f32)wf->tex[(sint32)texture_pick(ray)].width);
-	texture_map(wf, &tex_map, x, ray, &wf->tex[tex_id]);
+	texture_map(wf, &tex_map, x, ray->perp_dist, &wf->tex[tex_id]);
 }
